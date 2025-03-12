122c5f72c925b401713dd34b590b651a766eb789
ranjithrb <ranjith0072007@gmail.com>
2025-03-12 14:26:29 +0530

    simple CRUD operations

--- src/index.js
+++ src/index.js
@@ -6,20 +6,66 @@ const userModel = require("./models/user");
 const PORT = 8081;
 const app = express();
 
+app.use(express.json());
 app.use(morgan("dev"));
 
+// Create a user
 app.post("/signup", async (req, res) => {
   try {
-    const userDoc = new userModel({
-      firstName: "Sachin",
-      lastName: "Tendulkar",
-    });
+    const userDoc = new userModel(req.body);
 
     await userDoc.save();
 
     res.send("User created successfully!");
   } catch (error) {
     res.status(500).send("User creation failed!");
+  }
+});
+
+// Get a user
+app.get("/user", async (req, res) => {
+  try {
+    const user = await userModel.findOne(req.body);
+
+    if (!user) return res.status(404).send("User not found!");
+    res.send(user);
+  } catch (error) {
+    res.status(500).send("User cannot be fetched now!");
+  }
+});
+
+// Get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send("Feed cannot be fetched now!");
+  }
+});
+
+// Delete a user
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const user = await userModel.findByIdAndDelete(userId);
+    if (!user) return res.status(404).send("User with the given ID not found!");
+    res.send(user);
+  } catch (error) {
+    res.status(500).send("User cannot be deleted now!");
+  }
+});
+
+// Update user details
+app.patch("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const user = await userModel.findByIdAndUpdate(userId, req.body);
+    if (!user) return res.status(404).send("User with the given ID not found!");
+    res.send(user);
+  } catch (error) {
+    console.log("err ", error);
+    res.status(500).send("User cannot be updated now!");
   }
 });
 
